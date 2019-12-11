8925fbf3962443b5327d5e98e40f6271f0fe1faa
Andrew Song <awhsg11@gmail.com>
2019-12-11 12:13:07 -0800

    review news not done

--- movieResult.js
+++ movieResult.js
@@ -54,32 +54,31 @@ class movieResult {
         $('#movieInfoModal').removeClass('hidden');
     }
 
-    getNewsInfo() {
-        var ajaxConfigObject = {
-            dataType: 'json',
-            url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=CL81LUbZFCEpOlFyAH0ejmIgf9l93Sqs",
-            method: 'GET',
-            success: this.newsResponse,
-            error: this.newsResponseError
-        };
-        $.ajax(ajaxConfigObject)
-    }
+    // getNewsInfo() {
+    //     var ajaxConfigObject = {
+    //         dataType: 'json',
+    //         url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=CL81LUbZFCEpOlFyAH0ejmIgf9l93Sqs",
+    //         method: 'GET',
+    //         success: this.newsResponse,
+    //         error: this.newsResponseError
+    //     };
+    //     $.ajax(ajaxConfigObject)
+    // }
 
-    newsResponse(response) {
-        if (response.response.length > 5) {
-            for (var index = 0; index < 5; index++) {
-                console.log(response.response.docs[index].web_url);
-                // var url = response.response.docs[index].web_url;
-            }
-        } else {
-            for (var index = 0; index < response.response.docs.length; index++) {
-                console.log(response.response.docs[index].web_url);
-                // var url = response.response.docs[index].web_url;
-            }
-        }
-    }
+    // newsResponse(response) {
+    //     if (response.response.length > 5) {
+    //         for (var index = 0; index < 5; index++) {
+    //             console.log(response.response.docs[index].web_url);
+    //             // var url = response.response.docs[index].web_url;
+    //         }
+    //     } else {
+    //         for (var index = 0; index < response.response.docs.length; index++) {
+    //             console.log(response.response.docs[index].web_url);
+    //             // var url = response.response.docs[index].web_url;
+    //         }
+    //     }
+    // }
 
-    newsResponseError(response) {
-        console.log(response);
+    // newsResponseError(response) {
+    //     console.log(response);
     }
-}
