9e5766a360f64c58e95aa4fb2368183965e7ba76
Alex Aranda <alexraranda@gmail.com>
2019-12-10 16:40:16 -0800

    added data attributes to poster div for easy access from modal

--- MNR.js
+++ MNR.js
@@ -1,12 +1,22 @@
 $(document).ready(initializeApp);
 
+
 function initializeApp(){
     $('#searchButton').on('click', getInfo);
+    $('.resultContainer').on('click', '.moviePoster', handleShowModal);
+    $('.modalCloseButton').on('click', handleCloseModal)
 }
 
 function getInfo(){
-var searchInputValue = $('#searchInput').val();
-console.log(searchInputValue);
-var movieSearch = new movieResult(searchInputValue);
+    var searchInputValue = $('#searchInput').val();
+    var movieSearch = new movieResult(searchInputValue);
 
+}
+
+function handleShowModal(){
+    $('#movieInfoModal').removeClass('hidden');
+}
+
+function handleCloseModal(){
+    $('#movieInfoModal').addClass('hidden');
 }
--- movieResult.js
+++ movieResult.js
@@ -1,53 +1,51 @@
-class movieResult{
-    constructor(movieToSearch){
+class movieResult {
+    constructor(movieToSearch) {
         $('.resultContainer').empty();
         this.getMovieInfo(movieToSearch);
     }
 
-        getMovieInfo(movieTitle){
-            var ajaxConfigObject = {
-                dataType: 'json',
-                url: "http://www.omdbapi.com/?i=tt3896198&apikey=6c2d937e",
-                method: 'GET',
-                data: {
-                    s: movieTitle
-                },
-                success: this.processOmdbResponse,
-                error: this.processOmdbError
-            };
-            $.ajax(ajaxConfigObject)
-        }
+    getMovieInfo(movieTitle) {
+        var ajaxConfigObject = {
+            dataType: 'json',
+            url: "http://www.omdbapi.com/?i=tt3896198&apikey=6c2d937e",
+            method: 'GET',
+            data: {
+                s: movieTitle
+            },
+            success: this.processOmdbResponse,
+            error: this.processOmdbError
+        };
+        $.ajax(ajaxConfigObject)
+    }
 
-        processOmdbResponse(response){
-            console.log(response);
-            for(var i=0; i<response.Search.length; i++){
-                if(response.Search[i].Poster === 'N/A'){
-                    var noPosterDiv = $('<div>').text(response.Search[i].Title).addClass('moviePoster');
-                    $('.resultContainer').append(noPosterDiv);
-                }else{
-                    var posterDiv = $('<div>').css('background-image', 'url(' + response.Search[i].Poster + ')').addClass('moviePoster');
-                    $('.resultContainer').append(posterDiv);
+    processOmdbResponse(response) {
+        console.log(response);
+        for (var i = 0; i < response.Search.length; i++) {
+            if (response.Search[i].Poster === 'N/A') {
+                var noPosterDiv = $('<div>').text(response.Search[i].Title).addClass('moviePoster');
+                $('.resultContainer').append(noPosterDiv);
+            } else {
+                var posterDiv = $('<div>').css('background-image', 'url(' + response.Search[i].Poster + ')').addClass('moviePoster').attr({'title': response.Search[i].Title}).attr({'type': response.Search[i].Type}).attr({'year': response.Search[i].Year});
+                $('.resultContainer').append(posterDiv);
 
-                }
             }
         }
-        // processOmdbResult(response){
-        //     console.log(response);
+    }
+    // processOmdbResult(response){
+    //     console.log(response);
 
-        //     for(var i=0; i<response.Search.length; i++){
-        //         // var posterDiv = $('<div>').attr('background-image', 'url(\"' + response.Search[i].Poster + '\")');
-        //         var posterDiv = $('<div>').css('background-image', 'url(' + response.Search[i].Poster + ')').addClass('moviePoster');
-        //         $('.resultContainer').append(posterDiv);
-        //     }
-        // }
+    //     for(var i=0; i<response.Search.length; i++){
+    //         // var posterDiv = $('<div>').attr('background-image', 'url(\"' + response.Search[i].Poster + '\")');
+    //         var posterDiv = $('<div>').css('background-image', 'url(' + response.Search[i].Poster + ')').addClass('moviePoster');
+    //         $('.resultContainer').append(posterDiv);
+    //     }
+    // }
 
-        processOmdbError(response){
-            console.log(response);
-        }
+    processOmdbError(response) {
+        console.log(response);
+    }
 
-
-
-
+    
 
 
 
